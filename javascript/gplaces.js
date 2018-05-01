0aba2d4b8ee1435d9bb6a7c85f23b48cc6c465d4
Rylie Johnson <paintedflame@gmail.com>
2018-04-30 19:54:23 -0500

    gets photos working

--- javascript/gplaces.js
+++ javascript/gplaces.js
@@ -55,12 +55,13 @@ function renderButtons() {
         for (var j = 0; j < results.length; j++){
             var result = results[j];
             var placeDiv = $("<div class='card'>");
-            //adds image
-            placeDiv.append("<div class='card-image'><img src='" + result.photos[0] + "' alt='place image'></div>");
+            
+            // //adds image
+            placeDiv.append("<div class='card-image'><img src='https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + result.photos[0].photo_reference + "&key=AIzaSyD0QSfHIgzXIakE7DMJpdq18X6A8X4OHy4' alt='place image'></div>");
             //adds name and type
             placeDiv.append("<header class='card-header'><p class='card-header-title'>" + result.name + "</p><p class='title is-6'>" + result.types);
             //adds other content
-            placeDiv.append("<div class='card-content'><div class='content'>Rating: " + result.rating + "<br>Address: " + result.formatted_address + "<br><a href='" + result.website + "'>Website</a></div></div>");
+            placeDiv.append("<div class='card-content'><div class='content'>Rating: " + result.rating + "<br>Address: " + result.formatted_address + "</div></div>");
             $("#google-places-here").append(placeDiv);
             console.log("Getting Gere");
         }
